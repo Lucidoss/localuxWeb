a9bbe0b2bc9159065424be424febfc43b30d3c8b
Enzo <bib.enzoarchambaud@gmail.com>
2023-04-12 11:49:24 +0200

    create API

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Destination;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    #[Route('/api/destination', name:'app_apiLesDestinations', methods:['GET'])]
+    public function apiLesDestinations(ManagerRegistry $doctrine): JsonResponse
+    {
+        $repository = $doctrine->getRepository(Destination::class);
+        $lesDestinations = $repository->findAll();
+        $tabDestinations=[];
+        foreach($lesDestinations as $laDestination) {
+            $tabDestinations[]=[
+                'id'=>$laDestination->getId(),
+                'libelle'=>$laDestination->getlibelle(),
+            ];
+        }
+        return new JsonResponse($tabDestinations, 200, ['Access-Control-Allow-Origin'=>'*']);
+    }
+}
