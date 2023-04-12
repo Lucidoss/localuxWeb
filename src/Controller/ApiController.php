78212cc5a3ac1dec62e3cc660ebc73b3f9a89055
Enzo <bib.enzoarchambaud@gmail.com>
2023-04-12 13:27:26 +0200

    create branch

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,7 +6,6 @@ use App\Entity\Destination;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends AbstractController
